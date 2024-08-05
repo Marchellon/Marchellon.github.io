40cef2ba12d9ade20bf75914cf3fbab71f87951e
Marchellon <96017644+Marchellon@users.noreply.github.com>
2024-08-05 16:08:42 -0300

    Update no script.js

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
     navLinks.forEach(link => {
         link.addEventListener('click', function() {
+
+            if (link.getAttribute('href') !== '#contact') {
+
+                document.querySelectorAll('.section').forEach(section => section.classList.remove('flash'));
+                
+                const targetId = link.getAttribute('href').substring(1);
+                const targetSection = document.getElementById(targetId);
+                if (targetSection) {
+                    targetSection.classList.add('flash');
+                }
+            }
+
             nav.classList.remove('active');
         });
     });
